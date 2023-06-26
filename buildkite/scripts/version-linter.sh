14dadb19174e753b41800f326ef3674a62fdf5be
dkijania <dariusz@o1labs.org>
2023-06-26 23:28:28 +0200

    added pr branch to script exec

--- buildkite/scripts/version-linter.sh
+++ buildkite/scripts/version-linter.sh
@@ -14,7 +14,8 @@ export LIBP2P_NIXLESS=1 PATH=/usr/lib/go/bin:$PATH GO=/usr/lib/go/bin/go
 time make build
 
 base_branch=${BUILDKITE_PULL_REQUEST_BASE_BRANCH}
+pr_branch=${BUILDKITE_BRANCH}
 release_branch=$1
 
 echo "--- Run Python version linter"
-./scripts/version-linter.py ${base_branch} ${release_branch}
+./scripts/version-linter.py ${pr_branch} ${base_branch} ${release_branch}
