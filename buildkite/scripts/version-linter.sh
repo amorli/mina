b5e3d45a9f3d85e844dc16272a3529df009e8538
dkijania <dariusz@o1labs.org>
2023-06-27 20:30:05 +0200

    no pushing data on build but on script level

--- buildkite/scripts/version-linter.sh
+++ buildkite/scripts/version-linter.sh
@@ -21,6 +21,16 @@ echo "deb [trusted=yes] http://packages.o1test.net $MINA_DEB_CODENAME $MINA_DEB_
 apt-get update
 apt-get install --allow-downgrades -y "mina-${TESTNET_NAME}=${MINA_DEB_VERSION}"
 
+TYPE_SHAPE_FILE=${MINA_COMMIT_SHA1:0:7}-type_shape.txt
+MAX_DEPTH=12
+
+echo "--- Create type shapes git note for commit: ${MINA_COMMIT_SHA1:0:7}"
+./_build/default/src/app/cli/src/mina_testnet_signatures.exe \
+    internal dump-type-shapes --max-depth ${MAX_DEPTH} > ${TYPE_SHAPE_FILE}
+
+echo "--- Uploading ${TYPE_SHAPE_FILE} to mina-type-shapes bucket for consumption by the version linter"
+gcloud storage cp ${TYPE_SHAPE_FILE} gs://mina-type-shapes
+
 base_branch=${BUILDKITE_PULL_REQUEST_BASE_BRANCH}
 pr_branch=${BUILDKITE_BRANCH}
 release_branch=$1
