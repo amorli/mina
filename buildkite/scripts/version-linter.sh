b62676663683d67294692d5b5a880ba1bc36caae
dkijania <dariusz@o1labs.org>
2023-06-27 19:33:53 +0200

    Additional logging when invoking version linter script. Downloading mina from deb rather than building it again

--- buildkite/scripts/version-linter.sh
+++ buildkite/scripts/version-linter.sh
@@ -7,15 +7,23 @@ if [[ $# -ne 1 ]]; then
     exit 1
 fi
 
-source ~/.profile
+apt-get update
+apt-get install -y git apt-transport-https ca-certificates tzdata curl
 
-echo "--- Make build"
-export LIBP2P_NIXLESS=1 PATH=/usr/lib/go/bin:$PATH GO=/usr/lib/go/bin/go
-time make build
+TESTNET_NAME="berkeley"
+
+git config --global --add safe.directory /workdir
+
+source buildkite/scripts/export-git-env-vars.sh
+
+echo "Installing mina daemon package: mina-${TESTNET_NAME}=${MINA_DEB_VERSION}"
+echo "deb [trusted=yes] http://packages.o1test.net $MINA_DEB_CODENAME $MINA_DEB_RELEASE" | tee /etc/apt/sources.list.d/mina.list
+apt-get update
+apt-get install --allow-downgrades -y "mina-${TESTNET_NAME}=${MINA_DEB_VERSION}"
 
 base_branch=${BUILDKITE_PULL_REQUEST_BASE_BRANCH}
 pr_branch=${BUILDKITE_BRANCH}
 release_branch=$1
 
-echo "--- Run Python version linter"
+echo "--- Run Python version linter with branches: ${pr_branch} ${base_branch} ${release_branch}"
 ./scripts/version-linter.py ${pr_branch} ${base_branch} ${release_branch}
