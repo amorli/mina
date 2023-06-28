0225d61703bc0810838ba23784a4095dc48e0774
dkijania <dariusz@o1labs.org>
2023-06-28 19:11:48 +0200

    removing duplicated mina.deb download

--- buildkite/scripts/version-linter.sh
+++ buildkite/scripts/version-linter.sh
@@ -7,22 +7,9 @@ if [[ $# -ne 1 ]]; then
     exit 1
 fi
 
-# Don't prompt for answers during apt-get install
-export DEBIAN_FRONTEND=noninteractive
-
-apt-get update
-apt-get install -y git apt-transport-https ca-certificates tzdata curl
-
-TESTNET_NAME="berkeley"
-
 git config --global --add safe.directory /workdir
 
 source buildkite/scripts/export-git-env-vars.sh
-
-echo "Installing mina daemon package: mina-${TESTNET_NAME}=${MINA_DEB_VERSION}"
-echo "deb [trusted=yes] http://packages.o1test.net $MINA_DEB_CODENAME $MINA_DEB_RELEASE" | tee /etc/apt/sources.list.d/mina.list
-apt-get update
-apt-get install --allow-downgrades -y "mina-${TESTNET_NAME}=${MINA_DEB_VERSION}"
 
 MINA_COMMIT_SHA1=$(git rev-parse HEAD)
 TYPE_SHAPE_FILE=${MINA_COMMIT_SHA1:0:7}-type_shape.txt
