3e54f4a86ba2f75b121115a9173128b6bee26780
dkijania <dariusz@o1labs.org>
2023-06-28 15:12:00 +0200

    proper git config

--- buildkite/scripts/download-mina-deb.sh
+++ buildkite/scripts/download-mina-deb.sh
@@ -14,12 +14,11 @@ TESTNET_NAME=$1
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
-apt-get install -y git
+apt-get install -y git apt-transport-https ca-certificates tzdata curl
+
+git config --global --add safe.directory /workdir
 
 source buildkite/scripts/export-git-env-vars.sh
-
-# Don't prompt for answers during apt-get install
-export DEBIAN_FRONTEND=noninteractive
 
 echo "Installing mina daemon package: mina-${TESTNET_NAME}=${MINA_DEB_VERSION}"
 echo "deb [trusted=yes] http://packages.o1test.net $MINA_DEB_CODENAME $MINA_DEB_RELEASE" | tee /etc/apt/sources.list.d/mina.list
