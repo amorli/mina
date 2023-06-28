c73e79143d8c8becfdb923f6a342c9509fd3761a
dkijania <dariusz@o1labs.org>
2023-06-28 22:59:38 +0200

    sudo echo and tee

--- buildkite/scripts/version-linter.sh
+++ buildkite/scripts/version-linter.sh
@@ -29,7 +29,7 @@ git config --global --add safe.directory /workdir
 source buildkite/scripts/export-git-env-vars.sh
 
 echo "Installing mina daemon package: mina-${TESTNET_NAME}=${MINA_DEB_VERSION}"
-echo "deb [trusted=yes] http://packages.o1test.net $MINA_DEB_CODENAME $MINA_DEB_RELEASE" | tee /etc/apt/sources.list.d/mina.list
+sudo echo "deb [trusted=yes] http://packages.o1test.net $MINA_DEB_CODENAME $MINA_DEB_RELEASE" | tee /etc/apt/sources.list.d/mina.list
 sudo apt-get update
 sudo apt-get install --allow-downgrades -y "mina-${TESTNET_NAME}=${MINA_DEB_VERSION}"
 
