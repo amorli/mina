337f586a334589d141e8c800bb61c89a35fd6245
dkijania <dariusz@o1labs.org>
2023-06-28 12:21:10 +0200

    use mina instead of mina_testnet_signature app to create shape file

--- buildkite/scripts/version-linter.sh
+++ buildkite/scripts/version-linter.sh
@@ -29,8 +29,7 @@ TYPE_SHAPE_FILE=${MINA_COMMIT_SHA1:0:7}-type_shape.txt
 MAX_DEPTH=12
 
 echo "--- Create type shapes git note for commit: ${MINA_COMMIT_SHA1:0:7}"
-./_build/default/src/app/cli/src/mina_testnet_signatures.exe \
-    internal dump-type-shapes --max-depth ${MAX_DEPTH} > ${TYPE_SHAPE_FILE}
+mina internal dump-type-shapes --max-depth ${MAX_DEPTH} > ${TYPE_SHAPE_FILE}
 
 echo "--- Uploading ${TYPE_SHAPE_FILE} to mina-type-shapes bucket for consumption by the version linter"
 gcloud storage cp ${TYPE_SHAPE_FILE} gs://mina-type-shapes
